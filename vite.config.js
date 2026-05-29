95eac8b5fe466bbed576460f2c7c2b44f074acf9
brianpzaide <brianpzaide@gmail.com>
2026-05-30 00:22:44 +0530

    fix base in vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'jat',
+  base: '/jat/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
